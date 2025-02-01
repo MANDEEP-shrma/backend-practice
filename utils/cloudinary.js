3ed13cbf80ea6fe68c9d90cdf0c8728e12147d0b
Mandeep Sharma <mandeepsharma0701@gmail.com>
2025-02-02 02:07:18 +0530

    Added Multer and Cloudinary

---
+++ utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) {
+            throw "The Url doesn't exist"
+        }
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been updated successfully
+        //this url is the public url where the file is uploaded 
+        console.log("file is uploaded on cloudinary",response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        //remove the locally saved temporary file as the upload operation got failed
+        return null
+    }
+}
+
+
+export {uploadOnCloudinary}
