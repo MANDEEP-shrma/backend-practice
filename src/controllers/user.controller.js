1dfbc0ba43db0c422c816b9827bdfccddc25731f
Mandeep Sharma <mandeepsharma0701@gmail.com>
2025-02-27 03:24:10 +0530

    Add getWatchHistory functionality to user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -412,4 +412,53 @@ const getUserChannelProfile = asyncHandlerMain(async (req, res) => {
 
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccount, updateAvatar, updateUserCoverImage };
+
+const getWatchHistory = asyncHandlerMain(async (req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        //ANCHOR - Sub pipeline
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1,
+                  }
+                }
+              ]
+            }
+          },{
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    },
+  ])
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200,user[0].watchHistory,"History fetched Successfully"))
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccount, updateAvatar, updateUserCoverImage ,getUserChannelProfile,getWatchHistory};
